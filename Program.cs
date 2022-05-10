96ec87a78e4bf0df2d67f9a859c0acfa04db5e29
GlebOgl <79518116+GlebOgl@users.noreply.github.com>
2022-05-10 22:22:48 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Game;
+using Game.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Map
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            MapModel.CreateMap();
+            Application.Run(new GameWindow());
+        }
+    }
+}
